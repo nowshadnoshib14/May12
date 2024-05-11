a0a43eed72dd5995799bfe3430cf8bd6d3155ca3
nowshad <nowshadnoshib1802@gmail.com>
2024-05-11 20:14:27 +0600

    second commit

---
+++ src/FourTwo/FourTwo.java
@@ -0,0 +1,12 @@
+package FourTwo;
+
+public class FourTwo {
+
+    public static void main(String[] args) {
+        int mystery = 1;
+        mystery = 1 - 2 * mystery;
+        mystery = mystery + 1;
+        System.out.println(mystery);
+    }
+
+}
