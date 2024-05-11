86570c9ff3b9b48e8751f7bbe17bfd520d8352bf
nowshad <nowshadnoshib1802@gmail.com>
2024-05-11 21:09:23 +0600

    fourth commit

---
+++ src/FourSeven/FourSeven.java
@@ -0,0 +1,20 @@
+package FourSeven;
+
+public class FourSeven {
+    public static void main(String[] args) {
+        double x = 2.5;
+        double y = -1.5;
+        int m = 18;
+        int n = 4;
+        double a = x + n * y - (x + n) * y;
+        double b = m / n + m % n;
+        double c = 5 * x - n / 5;
+        double d = 1 - (1 - (1 - (1 - (1 - n))));
+        double e = Math.sqrt(Math.sqrt(n));
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
