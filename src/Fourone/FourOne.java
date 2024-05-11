b6c42fb34094267e77377312e18ab4dd4baadb6b
nowshad <nowshadnoshib1802@gmail.com>
2024-05-11 20:03:29 +0600

    first commend

---
+++ src/Fourone/FourOne.java
@@ -0,0 +1,8 @@
+package Fourone;
+
+public class FourOne {
+    int daysPerWeek = 7;
+    int daysUntilEndOfSemester = 60;
+    final float CENTIMETERS_PER_INCH = 2.54f;
+    float heightOfTallestPerson = 180.5f;
+}
